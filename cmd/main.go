9b525fa2e77f4ef9e253e4e8ea1efa400e44ecbd
Anders F Björklund <anders.f.bjorklund@gmail.com>
2023-01-27 17:57:32 +0100

    Reorganize import and modules a bit

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/docker/machine/libmachine/drivers/plugin"
-	"github.com/machine-drivers/docker-machine-driver-qemu"
+	qemu "github.com/machine-drivers/docker-machine-driver-qemu"
 )
 
 func main() {
